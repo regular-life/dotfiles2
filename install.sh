d371f445c41f964e66cffdc0c4413ade05623429
sophos <godspeed-x@github.com>
2021-06-02 16:55:39 +0000

    testing some stuff
    Former-commit-id: 2e48178c6021cedc5b0ae49024e282d9a9c5cbab
    Former-commit-id: 8b38bf4b1bf8ec09f76713acf87cc5d3d7f77e85

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo $(pwd)
 current_dir=$(pwd)
 olddir=~/dotfiles_old             # old dotfiles backup directory
 home_files=$(ls ./dots/home/)
+echo $home_files
 config_files=$(ls ./dots/config/)
 mkdir $olddir
 mkdir $olddir/home
@@ -13,14 +14,15 @@ mkdir $olddir/config
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $home_files; do
     echo "Moving any existing dotfiles in home dir from ~ to $olddir/home"
-    mv ~/.$file ~/dotfiles_old/home
+    mv --verbose ~/.$file ~/dotfiles_old/home
     echo "Creating symlink to $file in home directory."
-    ln -s $current_dir/dots/home/$file ~/.$file
+    ln --verbose -s $current_dir/dots/home/$file ~/.$file
+    echo $(pwd)
 done
 
-for file in $config_files; do
-    echo "Moving any existing dotfiles from .config dir ~ to $olddir/config"
-    mv ~/.config/$file ~/dotfiles_old/home
-    echo "Creating symlink to $file in .config directory."
-    ln -s $current_dir/dots/config/$file ~/.config/$file
-done
+#for file in $config_files; do
+#    echo "Moving any existing dotfiles from .config dir ~ to $olddir/config"
+#    mv ~/.config/$file ~/dotfiles_old/home
+#    echo "Creating symlink to $file in .config directory."
+#    ln -s $current_dir/dots/config/$file ~/.config/$file
+#done
