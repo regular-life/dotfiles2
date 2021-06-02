8296c28fcdf85956ff73da9287eed1e7cf7bac12
sophos <godspeed-x@github.com>
2021-06-02 15:28:50 +0000

    remove pulseaudio stuff, use alsa directly instead
    Former-commit-id: e3c66c8791950219121d5bde39564811a606dd1d
    Former-commit-id: 00cbfd9c96b5df0c910b9df722fd80e84d1b9247

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -6,11 +6,11 @@
 # $./volume.sh mute
 
 function get_volume {
-    amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+    amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
 }
 
 function is_mute {
-    amixer -D pulse get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+    amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
 }
 
 function send_notification {
@@ -47,19 +47,19 @@ $DIR/notify-send.sh "$volume "$icon_name""     "" -t 2000 -h int:value:"$volume"
 case $1 in
     up)
 	# Set the volume on (if it was muted)
-	amixer -D pulse set Master on > /dev/null
+	amixer set Master on > /dev/null
 	# Up the volume (+ 5%)
-	amixer -D pulse sset Master 5%+ > /dev/null
+	amixer sset Master 5%+ > /dev/null
 	send_notification
 	;;
     down)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master 5%- > /dev/null
+	amixer set Master on > /dev/null
+	amixer sset Master 5%- > /dev/null
 	send_notification
 	;;
     mute)
     	# Toggle mute
-	amixer -D pulse set Master 1+ toggle > /dev/null
+	amixer set Master 1+ toggle > /dev/null
 	if is_mute ; then
     DIR=`dirname "$0"`
     $DIR/notify-send.sh "婢 x" --replace=555 -u normal "Mute" -t 2000
