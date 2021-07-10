29ed204b6c4a1235388f1d22cfa891569e40317f
godspeed-x <74945202+godspeed-x@users.noreply.github.com>
2021-07-11 01:31:16 +0530

    more functions to install more things (audio, yay, apps, dwm, etc.)
    Former-commit-id: 707b900f34193875ddadfb43d055ddf79c581b42
    Former-commit-id: 9cb4f3fa7ecc6dd8e6a2752a9fe3c0d449d12968

--- install_packages.sh
+++ install_packages.sh
@@ -6,7 +6,7 @@ install_cmd () {
     void install $1
   fi
   if [[ $os_name = "Arch Linux" ]];  then
-    sudo pacman -S $1
+    yay -S $1
   fi
 }
 
@@ -16,7 +16,7 @@ install_ohmyzsh () {
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
-  #install x and x related stuff
+# install x and x related stuff
 install_xorg () {
     install_cmd "xorg xbindkeys xsel xdotool"
   if [[ $os_name = "void" ]];  then
@@ -27,23 +27,45 @@ install_xorg () {
   fi
 }
 
-#install audio related stuff
+# install audio related stuff
 install_audio () {
-  install_cmd "alsa-utils pulseaudio volctl"
+  install_cmd "alsa-utils pulseaudio mpd volctl mpdris2 mpdas playerctl"
   sudo usermod -a -G audio $USER
 }
-setup_audio
 
-#install dwm
+# install dwm
 install_dwm () {
   cd $PWD/dwm-flexipatch
   sudo make clean install
 }
 
+# install yay
+install_yay () {
+  pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+}
+
+# install some basic useful apps
+install_apps () {
+  install_cmd "firefox"
+  install_cmd "vlc mpv cava ncmpcpp"  # multimedia
+  install_cmd "dunst pcmanfm lxappearance newsboat android"  # utilities
+
+  if [[ $os_name = "Arch Linux"]]; then
+    install_cmd "sublime-text-4 android-file-transfer android-tools"
+  elif [[ $os_name = "void" ]]; then
+    install_cmd "sublime-text4 android-file-transfer-linux android-tools"
+}       # dev stuff
 
 
 #######
+if [[ $os_name = "Arch Linux" ]];  then
+   install_yay 
+fi
 install_xorg
 install_ohmyzsh
 install_audio
 install_dwm
+
