c6c7df6f929371786c98e5b93ad23c2c457d5752
godspeed-x <74945202+godspeed-x@users.noreply.github.com>
2021-07-10 23:07:23 +0530

    change install script according to e664bb75b94ea8e994677ffdfe72a5dec29f29c7 [formerly d46c5297a9968edbacbe82d3f73b56fb4b3736c3] [formerly ac71393325bed01b7a6fa8358f52e8115b9e87cf], dd1e8ca522d8d4c29b991e4daeb8a87d03c17a7c [formerly ecd8f4928a5b6570f15bf9c9c5c3c2c98ebcf55b] [formerly 6cd9ebe59f2be42e099ea35e79f962b20b768974], 52ebe6a7b91ef9a2331ec36b5fccfc513fa0b7cb [formerly e856629724a00328d9d20452b48893eb6e7b9c04] [formerly 233b2c28c344e8685db3520a53643fe3bebd934d], 746e13e353d8ba36ed299754050a905678ccdc3e [formerly 6915c55e143f5fc9211617b87d69c015b1a3dde3] [formerly 42db7b6f9b4b6b3354549e9690a7187e21275ed1]
    Former-commit-id: 741a45755ab4e8e7ec80646e8915265edb42b615
    Former-commit-id: 62f4b4f072cdc8e8445bff85d93e8319979fcc8b

--- install.sh
+++ install.sh
@@ -4,28 +4,31 @@ echo $(pwd)
 
 current_dir=$(pwd)
 olddir=~/dotfiles_old             # old dotfiles backup directory
-home_files=$(ls ./dots/home/)
-config_files=$(ls ./dots/config/)
-echo $config_files
+# home_files=$(ls ./dots/home/)
+dotfiles=$(ls ./dots/)
+echo $dotfiles
 mkdir $olddir
-mkdir $olddir/home
-mkdir $olddir/config
+# mkdir $olddir/config
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $home_files; do
-    echo "Moving any existing dotfiles in home dir from ~ to $olddir/home"
-    mv --verbose ~/.$file ~/dotfiles_old/home
-    echo "Creating symlink to $file in home directory."
-    ln --verbose -s $current_dir/dots/home/$file ~/.$file
-    echo $(pwd)
-done
+# for file in $home_files; do
+#     echo "Moving any existing dotfiles in home dir from ~ to $olddir/home"
+#     mv --verbose ~/.$file ~/dotfiles_old/home
+#     echo "Creating symlink to $file in home directory."
+#     ln --verbose -s $current_dir/dots/home/$file ~/.$file
+#     echo $(pwd)
+# done
 
-for file in $config_files; do
+for file in $dotfiles; do
     echo "Moving any existing dotfiles from .config dir ~ to $olddir/config"
     mv --verbose ~/.config/$file ~/dotfiles_old/home
     echo "Creating symlink to $file in .config directory."
     ln --verbose -s $current_dir/dots/config/$file ~/.config/$file
 done
+
+mv ~/.zshrc $olddir/ 
+mv ~/.xinitrc $olddir/ 
+mv ~/.xbindkeysrc $olddir/
 
 os_name=$(grep '^NAME=' /etc/os-release | grep -o '".*"' | tr -d '"')
 if [[ $os_name = "void" ]] ; then
