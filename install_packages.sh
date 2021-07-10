67eba06fd3ef29cb61fbc0568394c6ac6bd791df
godspeed-x <74945202+godspeed-x@users.noreply.github.com>
2021-07-11 01:10:14 +0530

    organize stuff in functions
    Former-commit-id: af08f12af411fb9b6e89fc23a04d82abd6265b31
    Former-commit-id: 95576a32d7932b464348c9cbb72732e3108f57c9

--- install_packages.sh
+++ install_packages.sh
@@ -16,21 +16,34 @@ install_ohmyzsh () {
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
-#install x and x related stuff
-install_cmd "xorg xbindkeys xsel xdotool"
-if [[ $os_name = "void" ]];  then
-  install_cmd "xinit"
-fi
-if [[ $os_name = "Arch Linux" ]];  then
-  install_cmd "xorg-xinit"
-fi
+  #install x and x related stuff
+install_xorg () {
+    install_cmd "xorg xbindkeys xsel xdotool"
+  if [[ $os_name = "void" ]];  then
+    install_cmd "xinit"
+  fi
+  if [[ $os_name = "Arch Linux" ]];  then
+    install_cmd "xorg-xinit"
+  fi
+}
 
 #install audio related stuff
-setup_audio () {
+install_audio () {
   install_cmd "alsa-utils pulseaudio volctl"
   sudo usermod -a -G audio $USER
 }
 setup_audio
 
 #install dwm
+install_dwm () {
+  cd $PWD/dwm-flexipatch
+  sudo make clean install
+}
 
+
+
+#######
+install_xorg
+install_ohmyzsh
+install_audio
+install_dwm
